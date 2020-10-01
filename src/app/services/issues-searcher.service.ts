2f7aa8836974339c725d5f3730e6f00f513a1d0a
Emmanuel Plata <platae@sssirsa.org>
2020-09-30 22:32:36 -0500

    GET por número de issue

--- src/app/services/issues-searcher.service.ts
+++ src/app/services/issues-searcher.service.ts
@@ -10,6 +10,15 @@ export class IssuesService {
     constructor(private httpClient: HttpClient) { }
 
     /**
+         * This function gets a specific issue of a GitHub repo given the owner, repo name and issue id
+         * @param owner 
+         * @param repo 
+         */
+    public getIssue(issueNumber:number, owner: string = 'facebook', repo: string = 'react') {
+        return this.httpClient.get(`${URLS.api}/repos/${owner}/${repo}/issues/${issueNumber}`);
+    }
+
+    /**
      * This function gets the issues of a GitHub repo given the owner and repo name
      * @param owner 
      * @param repo 
